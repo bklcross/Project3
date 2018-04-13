b19b4272519c83aedf5454c8b8c817483d61029c
waynesong21 <waynesong21@gmail.com>
2018-04-12 22:23:33 -0700

    Jumbotron clean up and service summary containers

---
+++ src/components/MoreButton/MoreButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import "./MoreButton.css";
+
+
+const MoreButton = (props) => {
+
+
+    
+    return (
+        <Button outline color="secondary">More</Button>
+    );
+}
+export default MoreButton;

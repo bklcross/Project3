dc63fd35d7295417b98bb2e49fa8612ca1889606
waynesong21 <waynesong21@gmail.com>
2018-04-16 21:08:31 -0700

    introduction part and blog previews. Everything I worked so far is for the home page.

---
+++ src/components/BlogList/BlogPreview.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "../Grid";
+import "./BlogPreview.css";
+
+export const BlogPreview = props => (
+    <Container>
+        <Row>
+            <div className="blog1">
+                <img src="https://images.unsplash.com/photo-1496098868818-75736fc02eeb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e7812dba20298669a5208331af3bf5c5&auto=format&fit=crop&w=800&q=60" width="100%" height="100%"></img>
+                <Col size="md-8">
+                    <h3><i>Blog #1</i></h3>
+                    <p>This is a blog about blogs</p>
+                    <p className="arrow">&#x2192;</p>
+                {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+                </Col>
+            </div>
+            <div className="blog2">
+                <img src="https://images.unsplash.com/photo-1519427658477-aaac2cb5d4e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c798109419cbb9a5b46ebc371d7230f4&auto=format&fit=crop&w=800&q=60"></img>
+                <Col size="md-8">
+                    <h3><i>Blog #2</i></h3>
+                    <p>This is a blog about blogs</p>
+                    <p className="arrow">&#x2192;</p>
+                {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+                </Col>
+            </div>
+            <div className="blog3">
+                <img src="https://images.unsplash.com/photo-1522093243371-296c79a66df4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c5df6bb1f7e73b7621f64a4da294fea0&auto=format&fit=crop&w=800&q=60"></img>
+                <Col size="md-8">
+                    <h3><i>Blog #3</i></h3>
+                    <p>This is a blog about blogs</p>
+                    <p className="arrow">&#x2192;</p>
+                {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+                </Col>
+            </div>
+      </Row>
+    </Container>
+);
---
+++ src/components/BlogList/BlogPreviewItem.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import { Container, Row, Col } from "../Grid";
+
+// export const BlogPreviewItem = props => (
+//     <Container>
+//         <Row>
+//         <div className="image1">
+//         <Col size="xs-4 sm-4">
+//           <h3>Blog #1</h3>
+//           <p>This is a blog about blogs</p>
+//           {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+//         </Col>
+//         </div>
+//         <div className="image2">
+//             <Col size="xs-4 sm-4">
+//             <h3>Blog #2</h3>
+//             <p>This is a blog about blogs</p>
+//             {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+//             </Col>
+//         </div>
+//         <div className="image3">
+//             <Col size="xs-4 sm-4">
+//             <h3>Blog #1</h3>
+//             <p>This is a blog about blogs</p>
+//             {/* <a rel="noreferrer noopener" target="_blank" href={props.href}>Go to recipe!</a> */}
+//             </Col>
+//         </div>
+//       </Row>
+//     </Container>
+// );

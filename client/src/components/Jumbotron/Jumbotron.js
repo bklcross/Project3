59fac3d3b8b51582d56c21bedd4d89d3c4d7b238
Brian Lee <bklcross@gmail.com>
2018-04-25 21:14:53 -0700

    Services Update

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -12,13 +12,10 @@ const Jumbo = (props) => {
                 {/* <video id="bg-video" autoPlay="true" loop="loop" preload="metadata" muted="muted">
                 <source src="https://gcs-vimeo.akamaized.net/exp=1523608978~acl=%2A%2F708213662.mp4%2A~hmac=c97ace9c9f547d7150b8575d45650116de618b2e7e6b5c44491dea2bd0d98da5/vimeo-prod-skyfire-std-us/01/1512/8/207561527/708213662.mp4" type="video/ogg" />
                 </video> */}
-                <h1 className="display-3" id="jumboh">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 className="display-3" id="jumboh">Jessica Ko Therapy</h1>
                 <hr className="my-2" />
-                <p className="lead">It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                <Button color="primary">Learn More</Button>
-                </p>
+                {/* <p className="lead">My mission is to be an ambassador for healing and restoration by collaborating with couples and individuals to find wholeness and newness. I partner with clients to find identity, confidence, and comfort in their relationships and in themselves. And I work to foster the understanding of personal and relational history, culture, and contexts to breathe life into the present. </p> */}
+
             </div>
           </div>
         </div>

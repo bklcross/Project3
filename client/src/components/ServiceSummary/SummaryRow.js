1f81f1521160f70fa5fc977b0bf2a6f125cda3e3
Brian Lee <bklcross@gmail.com>
2018-04-26 00:59:38 -0700

    pakage optimization

--- client/src/components/ServiceSummary/SummaryRow.js
+++ client/src/components/ServiceSummary/SummaryRow.js
@@ -10,7 +10,6 @@ const SummaryRow = () => {
     return(
       <div className="row" id="serviceSummary">
           <Row>
-<<<<<<< HEAD
             <div className="col-12 col-lg-2 wow fadeInLeft">
                 <article data-aos="fade-up" data-aos-delay="3000" id="blog1">
                     <h2>Multicultural Couples</h2>
@@ -30,30 +29,6 @@ const SummaryRow = () => {
                    <h2>Asian American Issues</h2>
                    <p>Use therapy to learn how to cope with issues that are so specific to your 
                    experience, and begin to live a life of peace and harmony in your own unique cultural context.</p>
-=======
-            <Col size="md-2">
-                <article data-aos="fade-up" data-aos-delay="3000">
-                    <h2>Multicultural Couples</h2>
-                    <p> I can support you and your partner to create and develop a harmonious and unique culture that will support your relationship to thrive.</p>
-                </article>
-            </Col>
-            <Col size="md-2">
-                <article>
-                    <h2>Couples Therapy</h2>
-                    <p>Learn about yourself and your partner, and how you can be in an enriching and nurturing relationship as your most intimate and vulnerable self.</p>
-                </article>
-            </Col>
-            <Col size="md-2">
-                <article>
-                    <h2>Asian American Issues</h2>
-                    <p>Use therapy to learn how to cope with issues that are so specific to your experience, and begin to live a life of peace and harmony in your own unique cultural context.</p>
-                </article>
-            </Col>
-            <Col size="md-2">
-                <article>
-                    <h2>Families Dealing with Cancer</h2>
-                    <p>Therapy can help regain control over how the cancer affects the relationships that are the dearest to you.</p>
->>>>>>> master
                 </article>
             </div>
             <div className="col-12 col-lg-2 wow fadeInLeft" data-wow-delay="1.0s">
